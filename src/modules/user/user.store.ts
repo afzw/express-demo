ee6858a1a9f284a7bd684bea30ac238dc8e80240
AlexFun <fanzhiwen@forensix.cn>
2023-01-31 16:52:05 +0800

    test: 测试代码风格校验

--- src/modules/user/user.store.ts
+++ src/modules/user/user.store.ts
@@ -1,9 +1,8 @@
 const UserStore = {
-    /**
-     * 用户主页属性键名。
-     */
-    theProfileKeys: () => ['email', 'username', 'nickname', 'avatar', 'roles']
+  /**
+   * 用户主页属性键名。
+   */
+  theProfileKeys: () => ["email", "username", "nickname", "avatar", "roles"],
+};
 
-}
-
-export default UserStore
+export default UserStore;
