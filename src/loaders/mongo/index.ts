a49904ebc6f6353ec768540995798ada0bcfd0f7
AlexFun <fanzhiwen@forensix.cn>
2023-02-12 10:54:16 +0800

    fix: 修复mongoose6的strictQuery问题

--- src/loaders/mongo/index.ts
+++ src/loaders/mongo/index.ts
@@ -8,6 +8,13 @@ async function loadMongoDB(mongoConifg: App.Config['mongo']) {
 
   logger.print(6, `开始连接 Mongo 数据库: ${uri.replace(/:.*@/, ':****@')}`, { label: 'MONGO' })
 
+  /**
+   * mongoose6中，strictQuery选项是默认开启的。
+   * 此时若查询对象中出现数据模型中不存在的属性，那么会返回所有该模型下所有的mongoose文档。
+   * 这显然是很不合理且极其危险的，所以此处禁用该选项，等到升级mongoose7之后，该选项会默认关闭，届时可以删除此处代码。
+   */
+  mongoose.set('strictQuery', false)
+
   // eslint-disable-next-line no-constant-condition
   while (true) {
     // 尝试解决 topology was destroyed
