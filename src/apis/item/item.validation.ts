d36aec6ec883b5a12fd495dba10289c664eed77c
fanzhiwen <fanzhiwen@forensix.cn>
2023-06-07 14:27:04 +0800

    fix: little fix

--- src/apis/item/item.validation.ts
+++ src/apis/item/item.validation.ts
@@ -3,7 +3,8 @@ import Joi from 'joi'
 const ItemValidation = {
   createItem: {
     body: Joi.object({
-      name: Joi.string().required().messages({ 'any.only': 'sadfasd' })
+      name: Joi.string().required(),
+      price: Joi.number().required()
     })
   }
 }
