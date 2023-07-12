386eb7c2c07bb089ed4ab91e58e42960041c7c0a
fanzhiwen <fanzhiwen@forensix.cn>
2023-07-12 15:00:34 +0800

    fix: 修复config文件

--- config/config.example.ts
+++ config/config.example.ts
@@ -25,16 +25,16 @@ const config: App.Config = {
 
 //  实际部署服务时，根据服务运行环境进行动态配置
 if (process.env.SESSION_NAME) config.session.name = process.env.SESSION_NAME
-if (process.env.SESSION_SECRET) config.session.name = process.env.SESSION_SECRET
-if (process.env.SESSION_EXPIRE_DATY) config.session.name = process.env.SESSION_EXPIRE_DATY
-if (process.env.SESSION_MAX_PER_USER) config.session.name = process.env.SESSION_MAX_PER_USER
+if (process.env.SESSION_SECRET) config.session.secret = process.env.SESSION_SECRET
+if (process.env.SESSION_EXPIRE_DATY) config.session.expireDays = Number(process.env.SESSION_EXPIRE_DATY)
+if (process.env.SESSION_MAX_PER_USER) config.session.maxPerUser = Number(process.env.SESSION_MAX_PER_USER)
 
-if (process.env.MONGO_NAME) config.session.name = process.env.MONGO_NAME
-if (process.env.MONGO_HOST) config.session.name = process.env.MONGO_HOST
-if (process.env.MONGO_PORT) config.session.name = process.env.MONGO_PORT
-if (process.env.MONGO_DEBUG) config.session.name = process.env.MONGO_DEBUG
-if (process.env.MONGO_USERNAME) config.session.name = process.env.MONGO_USERNAME
-if (process.env.MONGO_PASSWORD) config.session.name = process.env.MONGO_PASSWORD
-if (process.env.MONGO_URI) config.session.name = process.env.MONGO_URI
+if (process.env.MONGO_NAME) config.mongo.name = process.env.MONGO_NAME
+if (process.env.MONGO_HOST) config.mongo.host = process.env.MONGO_HOST
+if (process.env.MONGO_PORT) config.mongo.port = Number(process.env.MONGO_PORT)
+if (process.env.MONGO_DEBUG) config.mongo.debug = Boolean(process.env.MONGO_DEBUG)
+if (process.env.MONGO_USERNAME) config.mongo.username = process.env.MONGO_USERNAME
+if (process.env.MONGO_PASSWORD) config.mongo.password = process.env.MONGO_PASSWORD
+if (process.env.MONGO_URI) config.mongo.uri = process.env.MONGO_URI
 
 export default config
