57db6d6a2e56bf5fc2004b722845731f2e6766ff
fanzhiwen <fanzhiwen@forensix.cn>
2023-09-13 10:44:16 +0800

    feat: 根据提供的文件名称/文件路径，获取文件后缀

--- src/lib/fs/utils.ts
+++ src/lib/fs/utils.ts
@@ -9,6 +9,15 @@ export function getFileNameByPath(path: string): string {
 }
 
 /**
+ * 根据提供的文件名称/文件路径，获取文件后缀
+ * @param path 文件路径/名称
+ * @return 文件后缀
+ */
+export function getExtByPath(path: string): string {
+  return path.substring(path.lastIndexOf('.') + 1)
+}
+
+/**
  * 根据提供的文件路径，获取文件的目录路径。
  * @param path 文件路径
  * @returns 目录路径
