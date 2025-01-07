46b33663d9b99743bb6c4051511d1ef8906e23d1
afzw <fanzhiwencc@gmail.com>
2025-01-07 12:02:39 +0800

    feat: add script

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
-import express from "express"
-import { config, Config } from "./config/config"
-import * as http from "http"
-import { initApp } from "./boot/app"
-
 // ts编译之后不会处理ts文件中的路径映射，需要借助module-alias解析编译后js文件中的路径别名。
 /* eslint-disable @typescript-eslint/no-var-requires */
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const moduleAlias = require('module-alias')
 moduleAlias.addAlias('@', __dirname)
+
+import express from "express"
+import * as http from "http"
+import { config, Config } from "@/config/config"
+import { initApp } from "@/boot/app"
 
 async function launchServer(config: Config): Promise<http.Server> {
   const app: express.Express = express()
